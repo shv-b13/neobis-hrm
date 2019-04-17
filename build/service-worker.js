77f3c0c84b9d4c9186cd2adf7d0e9d266d77a15d
Aidana Aidlanova <44188496+Aidanaaidlanova@users.noreply.github.com>
2019-04-18 02:10:08 +0600

    change to new build

--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.d8ae5445d205ee5acf3297c352304aaf.js"
+  "/precache-manifest.65442bba401992048a4a6b22e1e28f1c.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("./index.html", {
+workbox.routing.registerNavigationRoute("/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
